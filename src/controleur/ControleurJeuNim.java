dbee084c60b2c7d7193fc482d8e0e50b57768698
TayyibDincer <tayyibdincer@gmail.com>
2024-03-12 23:31:50 +0100

    maj all cause work on wrong rep
    ajout de "ProjetCDA.pdf" dans .gitignore

--- src/controleur/ControleurJeuNim.java
+++ src/controleur/ControleurJeuNim.java
@@ -1,72 +1,102 @@
 package controleur;
-import modele.Joueur;
+
 import vue.Ihm;
-import modele.GrpTas;
+import modele.*;
 
 public class ControleurJeuNim {
+    /** L'Ihm */
     Ihm ihm;
+
+    /** Constructeur de la classe ControleurJeuNim
+     * @param i L'ihm*/
     public ControleurJeuNim(Ihm i){
         this.ihm=i;
     }
-public void jouer() {
-    int nb=ihm.nbTas();
-    String nom1=ihm.nomJoueur();
-    String nom2=ihm.nomJoueur(nom1);
+    public void jouer() {
+        /* Demande le nombre de tas "nb" le nom du joueur 1 "nom1" et le nom du joueur 2 "nom2" */
+        int nb=ihm.nbTas();
+        String nom1=ihm.nomJoueur();
+        String nom2=ihm.nomJoueur(nom1);
 
-    GrpTas tas=new GrpTas(nb);
-    Joueur joueur1=new Joueur(nom1);
-    Joueur joueur2=new Joueur(nom2);
+        /* Crée une instance de GrpTas "tas" */
+        GrpTas tas=new GrpTas(nb);
 
-    int[] tab=tas.creation();
-    Joueur joueurActuel = joueur1;
-    boolean continuer=true;
+        /* Crée des instances de Joueur pour les deux joueurs */
+        Joueur joueur1=new Joueur(nom1);
+        Joueur joueur2=new Joueur(nom2);
 
-    while (continuer) {
-        ihm.afficherTas(tab,nb);
-        int[] coup=ihm.ligneEtNbAllumette(joueurActuel.getNom());
+        /* Extrait un tableau de tas */
+        int[] tab=tas.creation();
 
-        boolean estRetiree = tas.retirer(coup[0], coup[1]);
-        if (!estRetiree) { /* throws. */
-            ihm.afficherErreur();
-            continue;
-        }
-        else {
-            if (tas.estPartieTerminee()) {
-                joueurActuel.partieGagnee();
+        Joueur joueurActuel = joueur1;
+        boolean continuer=true;
 
-                boolean h = ihm.rejouerOuQuitter();
-                if (h){ /* Ici le code pour relancer une partie */
-                    tas =new GrpTas(tas.getNbInitial());
+        /* Boucle qui gère les parties. La boucle est quittée si l'utilisateur décide de quitter le jeu */
+        while (continuer) {
+            /* Affiche le tas dans le terminal et récupère le coup 'm n' sous la forme d'un tableau à deux valeurs */
+            ihm.afficherTas(tab,nb);
+            int[] coup=ihm.ligneEtNbAllumette(joueurActuel.getNom());
 
-                    tab=tas.creation();
 
+            try {
+                /* On affiche une erreur si le coup est invalide */
+                boolean estRetiree = tas.retirer(coup[0], coup[1]);
+                if (!estRetiree) {
+                    ihm.afficherErreur();
                     continue;
                 }
-                else {/* Ici le code pour quitter le jeu et afficher le vaiqueur */
-                    continuer = false;
-                    ihm.afficherPartiesGagnees(joueur1.getNom(), joueur1.getNbPartieGagnee());
-                    ihm.afficherPartiesGagnees(joueur2.getNom(), joueur2.getNbPartieGagnee());
+                else { /* Si le coup est valide on teste si la partie est terminée */
+                    if (tas.estPartieTerminee()) {
+                        joueurActuel.partieGagnee();
 
-                    int gagnantJoueur1 = Joueur.jeuGagnee(joueur1, joueur2);
+                        /* True s'ils veulent rejouer et false sinon */
+                        boolean h = ihm.rejouerOuQuitter();
 
-                    if (gagnantJoueur1>0){
-                        ihm.afficherGagnant(joueur1.getNom());
-                    }
-                    else if (gagnantJoueur1<0) {
-                        ihm.afficherGagnant(joueur2.getNom());
-                    }
-                    else {
-                        ihm.afficherGagnant(joueur1.getNom(), joueur2.getNom());
+                        if (h){ /* Ici le code pour rejouer une partie */
+                            /* Réinitialise tas avec le nb d'allumettes donné au début du jeu */
+                            tas =new GrpTas(tas.getNb());
+
+                            /* Réinitialise le tableau d'int */
+                            tab=tas.creation();
+
+                            continue;
+                        }
+                        else { /* Ici le code pour quitter le jeu et afficher le vainqueur */
+                            /* On met continuer à false pour quitter la boucle while */
+                            continuer = false;
+
+                            /* On affiche le nombre de parties gagnées pour chaque joueur */
+                            ihm.afficherPartiesGagnees(joueur1.getNom(), joueur1.getNbPartieGagnee());
+                            ihm.afficherPartiesGagnees(joueur2.getNom(), joueur2.getNbPartieGagnee());
+
+                            /* Affichage du gagnant ou ex aequo */
+                            int gagnantJoueur1 = Joueur.jeuGagnee(joueur1, joueur2);
+
+                            if (gagnantJoueur1>0){
+                                ihm.afficherGagnant(joueur1.getNom());
+                            }
+                            else if (gagnantJoueur1<0) {
+                                ihm.afficherGagnant(joueur2.getNom());
+                            }
+                            else {
+                                ihm.afficherGagnant(joueur1.getNom(), joueur2.getNom());
+                            }
+                        }
                     }
                 }
+                /* changer de joueur à chaque tour de jeu */
+                if (joueurActuel.equals(joueur1)){
+                    joueurActuel=joueur2;
+                }
+                else{
+                    joueurActuel=joueur1;
+                }
             }
-        }
-        if (joueurActuel.equals(joueur1)){
-            joueurActuel=joueur2;
-        }
-        else{
-            joueurActuel=joueur1;
+            catch (ArrayIndexOutOfBoundsException e) {
+                ihm.afficherErreur(e.getMessage());
+            }
+
+
         }
     }
-}
 }
--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -1,13 +1,14 @@
 package modele;
 
 public class Joueur {
-    /**
-     *
-     * @param nom
-     */
-    private String nom;
+    /** Le nom du joueur */
+    private final String nom;
+
+    /** Le nombre de parties gagnées du joueur */
     private int nbPartieGagnee;
 
+    /** Le constructeur de la classe Joueur
+     * @param nom Le nom du Joueur */
     public Joueur(String nom)  {
        this.nom=nom;
        nbPartieGagnee = 0;
@@ -20,12 +21,14 @@ public class Joueur {
         return nbPartieGagnee;
     }
 
+    /** Ajoute 1 aux nombres de parties gagnées du joueur */
     public void partieGagnee() {
         nbPartieGagnee++;
     }
 
-    /* Cette fonction renvoie 0 si les deux joueurs sont à ex aequo,
-    1 si c'est joueur1 qui a gagné la partie ou -1 si c'est le joueur2. */
+    /** Cette fonction renvoie 0 si les deux joueurs sont à ex aequo, 1 si c'est joueur1 qui a gagné la partie, ou -1 si c'est le joueur2.
+     * @param joueur1 Le joueur 1
+     * @param joueur2 Le joueur 2*/
     public static int jeuGagnee(Joueur joueur1, Joueur joueur2) {
         if (joueur1.getNbPartieGagnee() > joueur2.getNbPartieGagnee()) {
             return 1;
